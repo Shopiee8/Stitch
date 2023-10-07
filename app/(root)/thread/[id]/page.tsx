863076410e7bd0c637bb91da436a7a96fc7d8371
alikhan26308 <alikhan26308@gmail.com>
2023-10-08 03:06:18 +0500

    Image Upload Options added, Ratings feature added, Gradient and Stich Name added

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -16,6 +16,7 @@ async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
   if (!user) return null;
 
+  
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
